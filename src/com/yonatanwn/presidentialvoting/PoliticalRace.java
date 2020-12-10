d9d8a50c160c3c4d4c51f60e5be35e1a5077a6de
YonatanWN <52720132+YonatanWN@users.noreply.github.com>
2020-12-10 16:19:21 -0500

    Race command is created

---
+++ src/com/yonatanwn/presidentialvoting/PoliticalRace.java
@@ -0,0 +1,28 @@
+package com.yonatanwn.presidentialvoting;
+
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+
+public class PoliticalRace {
+
+    private int maxAmountofCandidates;
+    private String positionName;
+    private ArrayList candidates;
+
+    public PoliticalRace(String positionName, int maxAmountOfCandidates){
+        this.positionName = positionName;
+        this.maxAmountofCandidates = maxAmountOfCandidates;
+        this.candidates = new ArrayList<Player>();
+    }
+    public void addCandidate(Player player){
+        candidates.add(player);
+    }
+    public void removeCandidate(Player player){
+        if(candidates.contains(player)){
+            candidates.remove(player);
+        }
+    }
+
+
+}
--- src/com/yonatanwn/presidentialvoting/Vote.java
+++ src/com/yonatanwn/presidentialvoting/Vote.java
@@ -1,16 +1,26 @@
 package com.yonatanwn.presidentialvoting;
 
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Vote extends JavaPlugin {
     @Override
     public void onEnable() {
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN+"[PresidentialVoting] has been enabled");
+        this.getCommand("race").setExecutor(new Race());
     }
 
     @Override
     public void onDisable() {
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN+"[PresidentialVoting] has been disabled");
     }
+
 }
+// Nominate A candidate
+// Candidate = Player on the server and vote count
+// Multiple Job races
+// vote on a existing candidate
+// Winner gets Presidential Perms
+
